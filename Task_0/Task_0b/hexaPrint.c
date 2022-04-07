e6c6d2be88c9f6ac3f131e710a12ae96f28a2726
Ron Levi <ronlv4@gmail.com>
2022-04-07 18:31:29 +0300

    task 1b

--- Task_0/Task_0b/hexaPrint.c
+++ Task_0/Task_0b/hexaPrint.c
@@ -5,8 +5,9 @@ int main(int argc, char** argv)
 {
     char c;
     FILE* input = fopen(argv[1], "r");
+    printf("%s", argv[1]);
     while ((c = fgetc(input)) != EOF)
     {
-        printf("%x ", c);
+        printf("%", 2, c);
     }
 }
