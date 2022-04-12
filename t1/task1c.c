ab06b00e4bd8bc5679a32f34601f11fe1df353c8
Ron Levi <ronlv4@gmail.com>
2022-04-12 16:56:40 +0300

    task1b works

--- t1/task1c.c
+++ t1/task1c.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct virus {
-    unsigned short sigSize;
-    unsigned char* sig;
-    char virusName[16];
+typedef struct virus
+{
+	unsigned short sigSize;
+	unsigned char *sig;
+	char virusName[16];
 } virus;
 
 typedef struct link link;
- 
-struct link {
-    link *nextVirus;
-    virus *vir;
+
+struct link
+{
+	link *nextVirus;
+	virus *vir;
 };
 
 struct fun_desc
@@ -20,100 +22,93 @@ struct fun_desc
 	char (*fun)(char);
 };
 
-void printHex(unsigned char* buffer, int length)
+void printHex(unsigned char *buffer, int length)
 {
 	int i;
 	for (i = 0; i < length; i++)
 	{
-		printf("%02X ", buffer[i]);
-    	}
-     	printf("\n");
+		printf("%02X ", ((int)buffer[i]) & 0xff);
+	}
+	printf("\n");
 }
 
-void readVirus (virus* vir, FILE* input)
+void readVirus(virus *vir, FILE *input)
 {
-	unsigned short  N = fgetc(input) | fgetc(input) << 8;
+	unsigned short N = fgetc(input) | fgetc(input) << 8;
 	vir->sigSize = N;
 	vir->sig = malloc(N);
 	fread(vir->sig, 1, N, input);
 	fread(vir->virusName, 1, 16, input);
 }
 
-void printVirus(virus* vir, FILE* output)
+void printVirus(virus *vir, FILE *output)
 {
 	fprintf(output, "Virus name: %s\n", vir->virusName);
 	fprintf(output, "Virus size: %d\n", vir->sigSize);
-	fprintf(output, "signature:\n%s", vir->sig);
+	fprintf(output, "signature:\n");
 	printHex(vir->sig, vir->sigSize);
-	free(vir->sig);
 }
 
-
-
-void list_print(link *virus_list, FILE* output)
+void list_print(link *virus_list, FILE *output)
 {
-    while (virus_list != NULL && virus_list->vir != NULL)
-    {
-        printVirus(virus_list->vir, output);
-        virus_list = virus_list->nextVirus;
-    }
-    
-
+	link* current_link = virus_list;
+	while (current_link != NULL && current_link->vir != NULL)
+	{
+		printVirus(current_link->vir, output);
+		current_link = current_link->nextVirus;
+		printf("\n");
+	}
 }
 
-link* list_append(link* virus_list, link* to_add)
+link *list_append(link *virus_list, link *to_add)
 {
-	printf("%p\n", to_add);
-	printf("%p\n", to_add->vir);
-	to_add->nextVirus = malloc(sizeof(link));
-    to_add->nextVirus = virus_list;
-    return to_add;
+	if (to_add == NULL)
+	{
+		return virus_list;
+	}
+	// to_add->nextVirus = malloc(sizeof(link));
+	to_add->nextVirus = virus_list;
+	return to_add;
 }
 
 void list_free(link *virus_list)
 {
-    while (virus_list != NULL && virus_list->vir != NULL)
-    {
-        free(virus_list->vir);
-        virus_list = virus_list->nextVirus;
-    }
+	while (virus_list != NULL && virus_list->vir != NULL)
+	{
+		free(virus_list->vir);
+		virus_list = virus_list->nextVirus;
+	}
 }
 
-void load_signatures(link* virus_list)
+link* load_signatures()
 {
-    printf("please enter a signatures file name: \n");
-	char ch;
-	FILE* sig_file;
-	char* file_name = malloc(10);
-    scanf("\n%s", file_name);
-	while ((ch = getchar()) != '\n' && ch != EOF);
+	printf("please enter a signatures file name: \n");
+	FILE *sig_file;
+	char *file_name = malloc(10);
+	link* virus_list = NULL;
+	// scanf("\n%s", file_name);
+	// while ((ch = getchar()) != '\n' && ch != EOF);
+	file_name = "signatures";
 	sig_file = fopen(file_name, "r");
-	int i = 0;
-	while(i < 10) // 
+	while (!feof(sig_file))
 	{
-		virus* vir = (virus*)malloc(sizeof(virus));
+		virus *vir = (virus *)malloc(sizeof(virus));
 		readVirus(vir, sig_file);
-		link* to_add = (link*)malloc(sizeof(link));
+		link *to_add = (link *)malloc(sizeof(link));
 		to_add->nextVirus = NULL;
 		to_add->vir = vir;
 		virus_list = list_append(virus_list, to_add);
-		printVirus(vir, stdout);
-		i++;
-		printf("%d ", i);
 	}
-	
+	return virus_list->nextVirus;
 }
 
-void print_signatures(link* virus_list)
+void print_signatures(link *virus_list)
 {
 	printf("printing signatures:\n");
-	// virus* vir = malloc(sizeof(virus));
-	printf("allocated memory for virus\n");
-	// readVirus(vir, sig);
-	// printVirus(vir, stdout);
+	list_print(virus_list, stdout);
 }
 
-void validate_input(int input, char* menu[], int length)
+void validate_input(int input, char *menu[], int length)
 {
 	printf("validating input: %d\n", input);
 	if (input >= 0 && input <= length - 1)
@@ -127,10 +122,9 @@ void validate_input(int input, char* menu[], int length)
 	}
 }
 
-void print_menu(char* menu[], size_t length)
+void print_menu(char *menu[], size_t length)
 {
 	int i;
-	printf("printing menu:\n");
 	for (i = 0; i < length; i++)
 	{
 		printf("%d) %s\n", i, menu[i]);
@@ -143,36 +137,36 @@ void detect_virus(char *buffer, unsigned int size, link *virus_list)
 
 }
 
-
 int main(int argc, char **argv)
 {
 	int user_input;
-    // char* buffer = malloc(1 << 10);
-	link* virus_list = malloc(sizeof(link));
-    char* menu[] = {"Load signatures", "Print signatures", "Detect viruses", "Quit"};
-    int length = sizeof(menu) / sizeof(menu[0]);
-    do
+	// char* buffer = malloc(1 << 10);
+	link *virus_list = NULL;
+	char *menu[] = {"Load signatures", "Print signatures", "Detect viruses", "Quit"};
+	int length = sizeof(menu) / sizeof(menu[0]);
+	do
 	{
 		print_menu(menu, length);
-        user_input = fgetc(stdin) - '0';
-        fflush(stdin);
-		printf("length %d\n" ,length);
+		user_input = fgetc(stdin) - '0';
+		char ch;
+		while ((ch = getchar()) != '\n' && ch != EOF);
 		validate_input(user_input, menu, length);
-        switch (user_input)
-        {
-        case 0:
-           load_signatures(virus_list);
-            break;
-        
-        case 1:
-            print_signatures(virus_list);
-        
-        case 2:
-            break;
+		switch (user_input)
+		{
+		case 0:
+			virus_list = load_signatures();
+			break;
 
-        default:
+		case 1:
+			print_signatures(virus_list);
+
+		case 2:
+			break;
+
+		default:
+			list_free(virus_list);
 			exit(0);
-        }
+		}
 	} while (1);
 }
 /*
