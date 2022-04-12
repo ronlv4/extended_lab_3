f95ba3a70db5afb2e10f5c109fc7536586f3bb97
Ron Levi <ronlv4@gmail.com>
2022-04-12 21:26:44 +0300

    task 1c could work

--- t1/task1c.c
+++ t1/task1c.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct virus
 {
@@ -21,6 +22,17 @@ struct fun_desc
 	char *name;
 	char (*fun)(char);
 };
+
+char *map(char *array, int array_length, char (*f)(char))
+{
+	char *mapped_array = (char *)(malloc(array_length * sizeof(char)));
+	int i;
+	for (i = 0; i < array_length; i++)
+	{
+		mapped_array[i] = f(array[i]);
+	}
+	return mapped_array;
+}
 
 void printHex(unsigned char *buffer, int length)
 {
@@ -51,7 +63,7 @@ void printVirus(virus *vir, FILE *output)
 
 void list_print(link *virus_list, FILE *output)
 {
-	link* current_link = virus_list;
+	link *current_link = virus_list;
 	while (current_link != NULL && current_link->vir != NULL)
 	{
 		printVirus(current_link->vir, output);
@@ -80,12 +92,12 @@ void list_free(link *virus_list)
 	}
 }
 
-link* load_signatures()
+link *load_signatures()
 {
 	printf("please enter a signatures file name: \n");
 	FILE *sig_file;
-	char *file_name = malloc(10);
-	link* virus_list = NULL;
+	char *file_name = malloc(100);
+	link *virus_list = NULL;
 	// scanf("\n%s", file_name);
 	// while ((ch = getchar()) != '\n' && ch != EOF);
 	file_name = "signatures";
@@ -134,13 +146,48 @@ void print_menu(char *menu[], size_t length)
 
 void detect_virus(char *buffer, unsigned int size, link *virus_list)
 {
+	char ch;
+	char *file_path = malloc(100);
+	printf("Please enter a file path: \n");
+	scanf("\n%s", file_path);
+	while ((ch = getchar()) != '\n' && ch != EOF)
+		;
+	FILE *f = fopen(file_path, "r");
+	fseek(f, 0, SEEK_END);
+	long fsize = ftell(f);
+	fseek(f, 0, SEEK_SET);
 
+	fread(buffer, fsize, 1, f);
+	fclose(f);
+	buffer[fsize] = 0;
+
+	for (size_t i = 0; i < fsize; i++)
+	{
+		printf("%X ",buffer[i]);
+	}
+	
+
+	link *list_iter = virus_list;
+	while (list_iter != NULL)
+	{
+		printf("comparing with virus %s\n", list_iter->vir->virusName);
+		if (memcmp(buffer, list_iter->vir->sig, size) == 0)
+		{
+			printVirus(list_iter->vir,stdout);
+		}
+		list_iter = list_iter->nextVirus;
+	}
+	
 }
 
 int main(int argc, char **argv)
 {
+	char* string = "Hello 01 02 03 04 05";
+	FILE *fp = fopen("testVirus", "w");
+	fwrite(string, 1, 20, fp);
+	exit(0);
 	int user_input;
-	// char* buffer = malloc(1 << 10);
+	char *buffer = malloc(1 << 10);
 	link *virus_list = NULL;
 	char *menu[] = {"Load signatures", "Print signatures", "Detect viruses", "Quit"};
 	int length = sizeof(menu) / sizeof(menu[0]);
@@ -149,7 +196,8 @@ int main(int argc, char **argv)
 		print_menu(menu, length);
 		user_input = fgetc(stdin) - '0';
 		char ch;
-		while ((ch = getchar()) != '\n' && ch != EOF);
+		while ((ch = getchar()) != '\n' && ch != EOF)
+			;
 		validate_input(user_input, menu, length);
 		switch (user_input)
 		{
@@ -159,8 +207,10 @@ int main(int argc, char **argv)
 
 		case 1:
 			print_signatures(virus_list);
+			break;
 
 		case 2:
+			detect_virus(buffer, 5, virus_list);
 			break;
 
 		default:
